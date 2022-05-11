49df51725643f9432fc78e61c518ca32f90d165f
jordylimaeng <jordylimaeng@github.com>
2022-05-11 00:04:54 -0300

    adding the forming layout to the project

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,36 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
- 
+
     return (
-        <Pagination/>
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    {/* 
+                    mb = margin bottom 
+                    col-*-* = refers to the bootstrap gridsystem, multiples of 12 eg.: col-xl-4 defines
+                    defines that each component will ocuppy on max 3, therefore 3 per row
+                    */}
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
